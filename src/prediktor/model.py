fd3a638aa6fe6dd637c7ec0a420976a66b27086e
kulisak12 <kulisak12lp@gmail.com>
2023-10-30 12:03:56 +0100

    Use accelerate

--- src/prediktor/model.py
+++ src/prediktor/model.py
@@ -5,12 +5,13 @@ from prediktor.config import Config
 
 tokenizer = AutoTokenizer.from_pretrained(Config.model_path)
 tokenizer_with_prefix = AutoTokenizer.from_pretrained(Config.model_path, use_prefix_space=True)
-model = AutoModelForCausalLM.from_pretrained(Config.model_path)
+model = AutoModelForCausalLM.from_pretrained(
+    Config.model_path,
+    device_map="auto",
+    torch_dtype=torch.bfloat16
+)
 generation_config = GenerationConfig(
     max_new_tokens=Config.max_length,
     pad_token_id=tokenizer.eos_token_id,
 )
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.eval()
-model.to(device)
+device = model.device
