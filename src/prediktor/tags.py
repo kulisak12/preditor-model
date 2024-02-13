0991a03018fbcf51fe182a64546d9e541a3665c0
kulisak12 <kulisak12lp@gmail.com>
2024-02-13 11:04:55 +0100

    Copy case when creating variants

--- src/prediktor/tags.py
+++ src/prediktor/tags.py
@@ -50,25 +50,34 @@ def tag(text: str) -> List[TaggedForm]:
     return result
 
 
-def generate_word_variations(form: TaggedForm) -> Set[str]:
+def generate_word_variations(original: TaggedForm) -> Set[str]:
     """Generate lemma forms to consider when changing a sentence.
 
     Only meaningful variations are considered.
     The original form will always be included in the result.
     """
-    original = {form.form}
-    if form.lemma is None or form.tag is None:
-        return original
+    original_result = {original.form}
+    if original.lemma is None or original.tag is None:
+        return original_result
     morpho = tagger.getMorpho()
-    wildcard = create_tag_wildcard(form.tag)
+    wildcard = create_tag_wildcard(original.tag)
     lemmas_forms = morphodita.TaggedLemmasForms()
-    morpho.generate(form.lemma, wildcard, GUESSER, lemmas_forms)
-    variantions = {
-        form.form
+    morpho.generate(original.lemma, wildcard, GUESSER, lemmas_forms)
+    variations = {
+        copy_case(form.form, original.form)
         for lemma_forms in lemmas_forms
         for form in lemma_forms.forms
     }
-    return variantions | original
+    return variations | original_result
+
+
+def copy_case(word: str, reference: str) -> str:
+    """Copy the case from the reference word to the word."""
+    if reference.isupper():
+        return word.upper()
+    if reference[0].isupper():
+        return word[0].upper() + word[1:]
+    return word
 
 
 def create_tag_wildcard(tag: str) -> str:
