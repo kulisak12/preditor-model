2f1a5ad141d95ad6a424bd3506d532117df516f2
kulisak12 <kulisak12lp@gmail.com>
2024-02-13 09:50:31 +0100

    Dijstra baseline

--- src/prediktor/replacement/dijkstra.py
+++ src/prediktor/replacement/dijkstra.py
@@ -95,3 +95,50 @@ def replace_dijkstra(
         for new_text, new_nlp, forms in zip(new_texts, new_nlps, num_forms):
             node = SearchNode(new_text, new_nlp, forms)
             open_nodes.append(node)
+
+
+def replace_dijkstra_baseline(rvg: ReplacementVariantsGenerator) -> str:
+    """Find best replacement using Dijkstra-inspired approach.
+
+    Keep track of the best NLP for each word. Calculate the score as the
+    difference between the best NLP and the current NLP.
+
+    Does not work well.
+    """
+    def update_baselines(
+        old_num_forms: int, new_num_forms: int, nlp_diff: float
+    ) -> None:
+        baseline_diff = baselines[new_num_forms] - baselines[old_num_forms]
+        reduce_amount = baseline_diff - nlp_diff
+        if reduce_amount > 0:
+            for i in range(new_num_forms, len(baselines)):
+                baselines[i] -= reduce_amount
+
+    def baseline_key(node: SearchNode) -> float:
+        return node.nlp - baselines[node.num_forms]
+
+    start_node = SearchNode("", 0, 0)
+    open_nodes = [start_node]
+    baselines = [0.0]
+    for _ in rvg.tagged_forms:
+        baselines.append(baselines[-1] + 1000.0)
+    finished_nodes: List[SearchNode] = []
+
+    while open_nodes and len(finished_nodes) < 10:
+        current = min(open_nodes, key=baseline_key)
+        open_nodes.remove(current)
+        variants, num_variant_forms = rvg.get_variants(current.num_forms, 1)
+        if num_variant_forms == 0:
+            finished_nodes.append(current)
+            continue
+        new_num_forms = current.num_forms + num_variant_forms
+
+        new_texts = [current.text + variant for variant in variants]
+        new_nlps = nlp.infer_nlp_batch(new_texts)
+        for new_text, new_nlp in zip(new_texts, new_nlps):
+            node = SearchNode(new_text, new_nlp, new_num_forms)
+            open_nodes.append(node)
+        best_nlp_diff = min(new_nlps) - current.nlp
+        update_baselines(current.num_forms, new_num_forms, best_nlp_diff)
+
+    return min(finished_nodes, key=baseline_key).text
