314f5280e2c81bdf547e7031b6cd8b9fa8d5271b
kulisak12 <kulisak12lp@gmail.com>
2024-03-19 11:29:35 +0100

    Infill: score resulting sentences

--- src/prediktor/infilling/end.py
+++ src/prediktor/infilling/end.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import Iterable, List, Set
 
+from prediktor import nlp
 from prediktor.config import Config
 from prediktor.model.model import Model
 
@@ -8,62 +9,56 @@ PROMPT = "Write a sentence such that it ends with:"
 
 def infill_between(model: Model, before_cursor: str, after_cursor: str) -> str:
     """Generate an infill between the given strings."""
-    prompt = format_infill_prompt(before_cursor, after_cursor)
-    decoded = beam_search(model, prompt, after_cursor)
-    outputs = [extract_output(text, before_cursor) for text in decoded]
-    best_output = get_best_output(outputs, after_cursor).rstrip()
-    return best_output
+    prompt = _format_infill_prompt(before_cursor, after_cursor)
+    decoded = _beam_search(model, prompt)
+    variants = list(_expand_prefixes(decoded))
+    final = [
+        _format_final_sentence(before_cursor, variant, after_cursor)
+        for variant in variants
+    ]
+    nlps = nlp.infer_nlp(model, final)
+    argmin = min(range(len(nlps)), key=nlps.__getitem__)
+    return variants[argmin]
 
 
-def format_infill_prompt(before: str, after: str) -> str:
+def _format_infill_prompt(before: str, after: str) -> str:
     """Create the prompt for the infill generation."""
     return f"{PROMPT} {after}\n{before}"
 
 
-def extract_output(text: str, before_cursor: str) -> str:
-    filled = text[text.find(before_cursor) + len(before_cursor):]
-    return filled[:filled.find("\n")]
+def _format_final_sentence(before: str, infill: str, after: str) -> str:
+    """Create the final sentence from the infill and the context."""
+    return before + infill + " " + after
 
 
-def get_best_output(outputs: List[str], after: str) -> str:
-    """Return the output that best matches the text after cursor.
+def _expand_prefixes(infill_texts: Iterable[str]) -> Set[str]:
+    """Generate all prefixes of the generated texts.
 
-    Favor outputs in the beginning of the list.
+    The prefix always ends with a word boundary.
+    The last word is not included since it is not certain that it is complete.
     """
-    # exact match
-    for output in outputs:
-        if output.endswith(after):
-            return output[:len(output) - len(after)]
-    # partial match
-    end_first_word = after.split()[0]
-    for output in outputs:
-        pos = output.find(end_first_word)
-        if pos != -1:
-            return output[:pos]
-    # no match
-    for output in outputs:
-        splits = output.split()
-        if len(splits) > 1:
-            return splits[0]
-    # no output
-    return ""
+    result: Set[str] = set()
+    for text in infill_texts:
+        words = text.split()
+        for i in range(1, len(words)):
+            result.add(" ".join(words[:i]))
+    return result
 
 
-def beam_search(
+def _beam_search(
     model: Model,
     input_text: str,
-    end: str = "",
 ) -> List[str]:
     input_ids = model.tokenizer.encode(input_text, return_tensors="pt").to(model.device)
-    num_end_tokens = len(model.tokenizer.encode(end, add_special_tokens=False))
     gen_ids = model.model.generate(
         input_ids,
-        max_new_tokens=num_end_tokens + Config.max_length,
+        max_new_tokens=Config.max_length,
         num_return_sequences=Config.num_beams,
         num_beams=Config.num_beams,
         num_beam_groups=(Config.num_beams + 1) // 2,
         diversity_penalty=20.0,
         pad_token_id=model.tokenizer.eos_token_id
     )
-    decoded_texts = model.tokenizer.batch_decode(gen_ids, skip_special_tokens=True)
-    return decoded_texts
+    infills_ids = gen_ids[:, input_ids.shape[-1] :]
+    decoded_infills = model.tokenizer.batch_decode(infills_ids, skip_special_tokens=True)
+    return decoded_infills
