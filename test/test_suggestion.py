237ab478bc46e4bc5b3de5ad6b2fcec92c12fa48
kulisak12 <kulisak12lp@gmail.com>
2024-04-21 14:11:10 +0200

    Add suggestions test

---
+++ test/test_suggestion.py
@@ -0,0 +1,29 @@
+import pytest
+
+from preditor import suggestion
+
+
+@pytest.mark.parametrize("input_lines, expected_output", [
+    ([], []),
+    ([""], []),
+    (["", "First line."], ["First line."]),
+    (["First line.", "Second line.", ""], ["First line.", "Second line."]),
+    (["First line.", "", "Second line."], ["First line."]),
+    (["First line.", "Second line.", "Third line."], ["First line.", "Second line.", "Third line."]),
+    (["", "First line.", "Second line."], ["First line.", "Second line."])
+])
+def test_get_first_paragraph(input_lines, expected_output):
+    assert suggestion._get_first_paragraph(input_lines) == expected_output
+
+
+@pytest.mark.parametrize("input_lines, expected_output", [
+    ([], []),
+    ([""], []),
+    (["", "First line."], ["First line."]),
+    (["First line.", "Second line.", ""], ["First line.", "Second line."]),
+    (["First line.", "", "Second line."], ["Second line."]),
+    (["First line.", "Second line.", "Third line."], ["First line.", "Second line.", "Third line."]),
+    (["", "First line.", "Second line."], ["First line.", "Second line."])
+])
+def test_get_last_paragraph(input_lines, expected_output):
+    assert suggestion._get_last_paragraph(input_lines) == expected_output
