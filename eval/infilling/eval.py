24cc41a5cf7c8fa225dbf07331c86bd77ea0a41c
kulisak12 <kulisak12lp@gmail.com>
2024-04-26 09:01:13 +0200

    Better infilling eval

--- eval/infilling/eval.py
+++ eval/infilling/eval.py
@@ -1,72 +1,91 @@
 #!/usr/bin/env python3
 
+import argparse
+import csv
 import dataclasses
-import sys
-from typing import Dict, List, TextIO
+import re
+import time
+from typing import Any, Dict, List, TextIO
 
-from preditor.infilling import blank, end, infilling
+from preditor.infilling import blank, end, infilling, selection
 from preditor.server import model
 
 
 @dataclasses.dataclass(frozen=True)
 class Example:
     before_cursor: str
-    after_cursor: str
     expected: str
+    after_cursor: str
 
 
-INFILLERS: Dict[str, infilling.InfillFunc] = {
-    "Blank": blank.infill_between,
-    "End": lambda m, b, a: end.infill_between(m, b, a, end.PROMPT_CS),
+GENERATE_FUNCS: Dict[str, infilling.GenerateFunc] = {
+    "blank": blank.generate_infills,
+    "end": end.generate_infills,
+}
+SELECT_FUNCS: Dict[str, infilling.SelectFunc] = {
+    "match": selection.select_by_match,
+    "score": selection.select_by_score,
 }
 
 
 def main() -> None:
-    is_debug = "--debug" in sys.argv
-    with open("sentences.txt") as file:
-        examples = read_examples(file)
-    for name, infiller in INFILLERS.items():
-        correct = evaluate(infiller, examples, is_debug)
-        total = len(examples)
-        print(f"{name}: {correct}/{total}")
+    parser = build_parser()
+    args = parser.parse_args()
+    evaluate(
+        args.dataset, args.generate, args.select,
+        args.max_length, args.num_variants, args.debug,
+    )
 
 
 def evaluate(
-    infiller: infilling.InfillFunc,
-    examples: List[Example],
-    is_debug: bool = False
-) -> int:
+    dataset: str, generate_funcname: str, select_funcname: str,
+    max_length: int, num_variants: int, is_debug: bool = False
+) -> None:
+    summary = args_summary(locals())
+    config = infilling.InfillingConfig(max_length=max_length, num_variants=num_variants)
+    generate_func = GENERATE_FUNCS[generate_funcname]
+    select_func = SELECT_FUNCS[select_funcname]
+    with open(dataset) as file:
+        examples = read_examples(file)
+
     correct = 0
+    start = time.time()
     for example in examples:
-        actual = infilling._infill(
-            model, infiller,
-            example.before_cursor + example.after_cursor,
-            len(example.before_cursor)
+        actual = infilling.infill(
+            model, example.before_cursor, example.after_cursor,
+            config, "cs", generate_func, select_func,
         )
         if actual == example.expected:
             correct += 1
         if is_debug:
-            print(f"{example.before_cursor}<{actual}>{example.after_cursor}")
-    return correct
+            print(f"{example.before_cursor}|{actual}|{example.after_cursor}")
+    end = time.time()
+    avg_time = (end - start) / len(examples)
+
+    print(f"{correct}/{len(examples)}  avg {avg_time:.3f}s  {summary}")
 
 
 def read_examples(file: TextIO) -> List[Example]:
-    examples: List[Example] = []
-    for line in file:
-        line = line.strip()
-        if line:
-            examples.append(make_example(line))
-    return examples
+    reader = csv.DictReader(file, delimiter="|")
+    return [Example(**row) for row in reader]
 
 
-def make_example(line: str) -> Example:
-    blank_start = line.find("<")
-    blank_end = line.find(">")
-    assert blank_start != -1 and blank_end != -1
-    before_cursor = line[:blank_start]
-    after_cursor = line[blank_end + 1:]
-    expected = line[blank_start + 1:blank_end]
-    return Example(before_cursor, after_cursor, expected)
+def args_summary(args: Dict[str, Any]) -> str:
+    return ",".join(
+        "{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v)
+        for k, v in args.items()
+    )
+
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", type=str)
+    parser.add_argument("--generate", choices=GENERATE_FUNCS.keys(), required=True)
+    parser.add_argument("--select", choices=SELECT_FUNCS.keys(), required=True)
+    parser.add_argument("--max-length", type=int, default=8)
+    parser.add_argument("--num-variants", type=int, default=4)
+    parser.add_argument("--debug", action="store_true")
+    return parser
 
 
 if __name__ == "__main__":
